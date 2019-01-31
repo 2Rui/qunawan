f2f715f959624de16ef1311545c40b64e90715b0
HanXiaoRui <2698632709@qq.com>
2019-01-31 10:43:37 +0800

    recommend and weekend

---
+++ src/pages/home/components/Recomend.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <div class="cont">
+            <div class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                    <img :src="item.imgUrl" class="img-box"/>
+                <div class="right">
+                    <p class="right-title">{{item.title}}</p>
+                    <p class="desc">{{item.desc}}</p>
+                    <button>详情</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeReco',
+  data () {
+      return {
+          recommendList: [
+              {
+            id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg',
+            title: '故宫',
+            desc: '东方宫殿建筑代表，世界宫殿建筑典范'
+        }
+          ]
+      }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+ @import '~@styles/mixins.styl'
+      .title
+         line-height:.4rem
+         text-indent:.2rem
+         background:#eee
+         padding:.2rem
+     .cont
+         width:100%
+         .item
+             width:100%
+             height:1.9rem
+             display:flex
+             .img-box
+                 width:1.7rem
+                 height:1.7rem
+                 padding:0.1rem
+              .right
+                  flex:1
+                  display:flex
+                  flex-direction:column
+                  padding:.1rem
+                  overflow:hidden
+                  .right-title
+                     line-height:.54rem
+                     font-size:.32rem
+                     font-weight:bold
+                     ellipsis()
+                   .desc
+                       line-height:.4rem
+                       color:#ccc
+                    button 
+                      background:#ff9300
+                      border-radius:.06rem
+                      padding :.1rem
+                      color :#fff
+                      margin-top :.1rem
+                      line-height :.44rem
+                      width:2rem
+</style>
+
+
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="cont">
+        <div class="title">周末去那玩</div>
+        <div class="item">
+            <img src="http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg" class="item-img"/>
+            <div class="info">
+            <p class="item-title">北京温泉排行榜</p>
+            <p class="item-desc">细数北京温泉，温暖你的冬天</p>
+            </div>
+           
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@styles/mixins.styl'
+.cont 
+   width:100%
+   .title
+      line-height:.4rem
+      text-indent:.2rem
+      background:#eee
+      padding:.2rem
+     .item
+        display:flex
+        flex-direction :column
+        justify-content :center
+        .item-img
+           width:100%
+        .info
+           padding:.2rem
+           .item-title
+                line-height:.54rem
+                font-size:.32rem
+                font-weight:bold
+                ellipsis()
+            .item-desc
+                line-height:.4rem
+                color:#ccc
+</style>
